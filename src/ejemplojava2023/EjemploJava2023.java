e9c7435a7b826ccf24c07ca94e7231aa2dd0cfa0
muriosh <75389767+muriosh@users.noreply.github.com>
2023-01-15 02:01:33 +0100

    Update EjemploJava2023.java
    Añadida respuesta de Blas a Epi

--- src/ejemplojava2023/EjemploJava2023.java
+++ src/ejemplojava2023/EjemploJava2023.java
@@ -14,7 +14,7 @@ public class EjemploJava2023 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(">Epi: ¡Hola Blas!");
+        System.out.println(">Epi: ¡Hola Blas!\n>Blas: ¡Hola Epi!\n");
     }
     
 }
